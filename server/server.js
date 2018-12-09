9730603d443657b6fe28231517bd9480b6f3f067
shivam123425 <shivam123425@gmail.com>
2018-12-09 23:56:03 +0530

    Add URI, fix  error

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI =
+  "mongodb://admin:admin123@ds115543.mlab.com:15543/lyricaldb";
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
