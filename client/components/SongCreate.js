85f50d60b89e1f23c0cad436b0bc075ede5e6b4b
shivam123425 <shivam123425@gmail.com>
2018-12-10 15:58:56 +0530

    Seperate out query, refetchQuery after mutation

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 import { Link, hashHistory } from "react-router";
+import fetchSongs from "../queries/fetchSongs";
 
 class SongCreate extends Component {
   constructor(props) {
@@ -14,7 +15,8 @@ class SongCreate extends Component {
       .mutate({
         variables: {
           title: this.state.title
-        }
+        },
+        refetchQueries: [{ query: fetchSongs }]
       })
       .then(() => hashHistory.push("/"));
   }
