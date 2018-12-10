709b9b06d9b4cf2eafa66f89bb5e169b977e1f8b
shivam123425 <shivam123425@gmail.com>
2018-12-10 14:41:48 +0530

    Add addSong mutation

--- client/index.js
+++ client/index.js
@@ -4,7 +4,8 @@ import ApolloClient from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 import SongList from "./components/SongList";
 import { Router, Route, hashHistory, IndexRoute } from "react-router";
-import App from './components/App'
+import App from "./components/App";
+import SongCreate from "./components/SongCreate";
 
 const client = new ApolloClient({});
 
@@ -14,6 +15,7 @@ const Root = () => {
       <Router history={hashHistory}>
         <Route path="/" component={App}>
           <IndexRoute component={SongList} />
+          <Route path="song/new" component={SongCreate} />
         </Route>
       </Router>
     </ApolloProvider>
